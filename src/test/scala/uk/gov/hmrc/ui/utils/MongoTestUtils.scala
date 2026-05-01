f0d2aa6323ddc8dbcfb2e656d619b548cd200f39
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-05-01 17:29:01 +0100

    Add FI manage reports journey UI tests

---
+++ src/test/scala/uk/gov/hmrc/ui/utils/MongoTestUtils.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.utils
+
+import uk.gov.hmrc.ui.mongo.*
+
+trait MongoTestUtils {
+
+  val databaseName = "crs-fatca-submission-frontend"
+  val userAnswers  = "user-answers"
+
+  def cleanUserAnswersCollection(): Unit =
+    MongoService.dropMongoCollection(databaseName, userAnswers)
+
+  def dropCollection(collectionName: String): Unit =
+    MongoService.dropMongoCollection(databaseName, collectionName)
+}
