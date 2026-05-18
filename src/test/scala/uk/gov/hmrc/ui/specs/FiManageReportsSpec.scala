426d58d23dc1b2a569534240c618b97f761fe74d
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-05-18 16:28:01 +0100

    DAC6-4273: Add page objects and manual test cases for first 3 CRS elections pages (contracts, dormant-accounts, thresholds)

--- src/test/scala/uk/gov/hmrc/ui/specs/FiManageReportsSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/FiManageReportsSpec.scala
@@ -55,8 +55,8 @@ class FiManageReportsSpec extends BaseSpec {
       When("The user clicks back to manage your CRS and FATCA reports")
       ManageReportsPage.clickBackToManageReports()
 
-//      Then("The user is back on the your FIs page") - will uncomment after DAC6-4270 merged
-//      YourFisPage.onPage()
+      Then("The user is on the FI management page")
+      FiManagementPage.onPage()
     }
 
     Scenario("FATCA - Void journey", ManualSubmissionTests, SoloTests) {
