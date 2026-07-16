82166bccf8dae03cccef5b25de102784c5f051d5
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-07-16 18:18:41 +0100

    DAC6-4379: automate sponsor uk-postcode page UI tests

--- src/test/scala/uk/gov/hmrc/ui/pages/SponsorUkPostcodePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/SponsorUkPostcodePage.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,17 +19,23 @@ package uk.gov.hmrc.ui.pages
 import org.openqa.selenium.By
 import uk.gov.hmrc.ui.utils.TestData
 
-object ReportingDetailsYearPage extends BasePage {
+object SponsorUkPostcodePage extends BasePage {
 
-  override val pageUrl: String = baseUrlManualSub + "/manual/report-details/year"
+  override val pageUrl: String = baseUrlManualSub + "/manual/sponsor/uk-postcode"
 
-  private val yearInput: By = By.id("value")
+  val postcodeInput: By   = By.id("value")
+  val manualEntryLink: By = By.linkText("Or enter the address manually")
 
-  def enterYearAndContinue(): this.type = {
+  def checkPage(): this.type = {
     onPage(pageUrl)
-    sendKeys(yearInput, TestData.reportingYear)
-    click(submitButtonId)
+    checkH1(s"What is the postcode for ${TestData.sponsorName}?")
     this
   }
 
+  def enterPostcodeAndFindAddress(): this.type = {
+    onPage(pageUrl)
+    sendKeys(postcodeInput, TestData.postCode)
+    click(submitButtonId)
+    this
+  }
 }
