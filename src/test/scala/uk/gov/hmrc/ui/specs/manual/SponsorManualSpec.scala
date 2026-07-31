b64c945127ab7e1246622205a7482edc1835cd7a
vigod-nc <vinay.godena@netcompany.com>
2026-07-31 09:39:10 +0100

    Scala-fmt

--- src/test/scala/uk/gov/hmrc/ui/specs/manual/SponsorManualSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/manual/SponsorManualSpec.scala
@@ -80,8 +80,8 @@ class SponsorManualSpec extends BaseSpec with SponsorJourneyHelper {
       When("The Sponsor is Non-UK based and continue")
       SponsorWhereBasedPage.selectNoAndContinue()
 
-      //And("The user entered the Non-UK address manually")
-      //SponsorNonUKAddressPage.enterAddressNonUK()
+      // And("The user entered the Non-UK address manually")
+      // SponsorNonUKAddressPage.enterAddressNonUK()
     }
   }
 }
