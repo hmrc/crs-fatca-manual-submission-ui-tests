e93ef3fbe065cdb45205ef143dd7427d9c1fcbb7
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-08-24 16:29:43 +0100

    DAC6-4436: Add page object and spec for cp-so individual/organisation page

---
+++ src/test/scala/uk/gov/hmrc/ui/specs/manual/CpSoManualSpec.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs.manual
+
+import uk.gov.hmrc.ui.pages.*
+import uk.gov.hmrc.ui.pages.manual.*
+import uk.gov.hmrc.ui.pages.manual.controllingpersonandsubstantialowners.*
+import uk.gov.hmrc.ui.specs.BaseSpec
+import uk.gov.hmrc.ui.specs.tags.*
+
+class CpSoManualSpec extends BaseSpec with ManualJourneyHelper {
+
+  Feature("CRS/FATCA Manual - CP-SO individual or organisation") {
+
+    Scenario(
+      "CP-SO - selecting Individual continues the journey",
+      ManualSubmissionTests,
+      SoloTests
+    ) {
+      Given("The user has reached the manual task list as FATCA")
+      navigateToTaskList("FATCA")
+
+      // TODO: Replace direct navigation below with the click-through once
+      // /manual/cp-so/account-holder is implemented.
+      When("They navigate directly to the individual or organisation page")
+      CpSoIndividualOrOrganisationPage.goToPage()
+
+      //      When("They open the cp-so account holder task")
+      //      ManualSendAReportIndexPage.clickCpSoAccountHolder()
+
+      Then("They see the individual or organisation page")
+      CpSoIndividualOrOrganisationPage.checkPage()
+
+      When("They select Individual and continue")
+      CpSoIndividualOrOrganisationPage.selectIndividualAndContinue()
+    }
+
+    Scenario(
+      "CP-SO - selecting Organisation continues the journey",
+      ManualSubmissionTests,
+      SoloTests
+    ) {
+      Given("The user has reached the manual task list as FATCA")
+      navigateToTaskList("FATCA")
+
+      // TODO: Replace direct navigation below with the click-through once
+      // /manual/cp-so/account-holder is implemented.
+      When("They navigate directly to the individual or organisation page")
+      CpSoIndividualOrOrganisationPage.goToPage()
+
+      //      When("They open the cp-so account holder task")
+      //      ManualSendAReportIndexPage.clickCpSoAccountHolder()
+
+      Then("They see the individual or organisation page")
+      CpSoIndividualOrOrganisationPage.checkPage()
+
+      When("They select Organisation and continue")
+      CpSoIndividualOrOrganisationPage.selectOrganisationAndContinue()
+    }
+
+    Scenario(
+      "CP-SO - page is not accessible under CRS regime",
+      ManualSubmissionTests,
+      SoloTests
+    ) {
+      Given("The user has reached the manual task list as CRS")
+      navigateToTaskList("CRS")
+
+      When("They navigate directly to the individual or organisation page")
+      CpSoIndividualOrOrganisationPage.goToPage()
+
+      Then("They see the page not found error")
+      PageNotFoundPage.checkPage()
+    }
+
+  }
+}
