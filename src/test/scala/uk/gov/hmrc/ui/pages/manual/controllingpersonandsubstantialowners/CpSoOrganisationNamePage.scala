7241e752885076bfbd6367fec8bff645a7cb3b8e
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2026-09-23 14:33:59 +0100

    DAC6-4472: Add pages and spec coverage for organisation-name and self-certification

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/manual/controllingpersonandsubstantialowners/CpSoOrganisationNamePage.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages.manual.controllingpersonandsubstantialowners
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.ui.pages.BasePage
+
+object CpSoOrganisationNamePage extends BasePage {
+
+  override val pageUrl: String = baseUrlManualSub + "/manual/cp-so/organisation-name"
+
+  private val nameInput: By = By.id("value")
+
+  def checkPage(): this.type = {
+    onPage(pageUrl)
+    checkH1("What is the name of the organisation?")
+    this
+  }
+
+  def enterNameAndContinue(name: String): this.type = {
+    onPage(pageUrl)
+    sendKeys(nameInput, name)
+    click(submitButtonId)
+    this
+  }
+}
